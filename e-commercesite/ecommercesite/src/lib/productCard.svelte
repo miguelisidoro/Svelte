4713883d0655d0ce9ab88f18fb4f70b2e32c7239
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2023-06-27 16:31:36 +0100

    Added e-commerce web site pages and layouts.

---
+++ e-commercesite/ecommercesite/src/lib/productCard.svelte
@@ -0,0 +1,13 @@
+<div class="card">
+    <header class="card-header"><h2>Product Name</h2></header>
+    <div class="card-body px-4">
+        Quantity: <strong>4</strong>
+    </div>
+    <div class="card-body px-4">
+        Price: €10
+    </div>
+    <div class="card-footer">
+        <button class="p-2 rounded variant-class-primary">Add</button>
+        <button class="p-2 rounded variant-class-error">Remove</button>
+    </div>
+</div>
