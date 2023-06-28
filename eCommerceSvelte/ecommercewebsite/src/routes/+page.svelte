eda446abe0bc4dd049b4dfc12160650f48606a48
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2023-06-28 10:53:45 +0100

    Continued dev

--- eCommerceSvelte/ecommercewebsite/src/routes/+page.svelte
+++ eCommerceSvelte/ecommercewebsite/src/routes/+page.svelte
@@ -1,13 +1,12 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout, the CSS import order is critical!</li>
-			<li><code class="code">/src/app.postcss</code> - minimal css to make the page full screen, may not be relevant for your project</li>
-			<li><code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents</li>
-		</ul>
+	<div class="grid grid-cols-3 gap-4">
+		<div class="col-span-3">
+			<h1>Svelte Kit 1.0 Store</h1>
+		</div>
+		<div class="col-span-3">
+			<button class="btn variant-filled-primary">Checkout with Stripe API</button>
+		</div>
 	</div>
 </div>
