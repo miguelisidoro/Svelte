b9e6c66440024e1f670a609d6a250ed4de5ed7d6
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2023-06-29 17:18:08 +0100

    Continued dev

--- eCommerceSvelte/ecommercewebsite/src/cart.ts
+++ eCommerceSvelte/ecommercewebsite/src/cart.ts
@@ -10,16 +10,20 @@ export const addToCart = (id:string) => {
     if (itemPosition !== -1) {
         // item is in the cart, add 1 to quantity
         const updatedItems = items.map((item) => {
-           if (item.id === id) {
-            return [...items, {id: id, quantity: item.quantity + 1}];
-           }
-           return item;
+            let itemToReturn;
+            if (item.id === id) {
+                itemToReturn = [...items, {id: id, quantity: item.quantity + 1}];
+                itemToReturn.splice(0, 1);
+                return itemToReturn;
+            }
+            return itemToReturn;
         });
         return updatedItems;
     } else {
         // item is not in the cart, add item to cart with quantity 1
         cartItems.update(() => {
-            return [...items, {id: id, quantity: 1}];
+            const itemToReturn = [...items, {id: id, quantity: 1}];;
+            return itemToReturn;
         });
     }
 }
@@ -29,14 +33,17 @@ export const removeFromCart = (id:string) => {
     const itemPosition = items.findIndex((item) => item.id === id);
     if (items[itemPosition]?.quantity - 1 === 0) {
         //remove from cart if we have quantity 0
-        items.slice(itemPosition, 1);
+        items.splice(itemPosition, 1);
     }
 
     const updatedItems = items.map((item) => {
+        let itemToReturn;
         if (item.id === id) {
-         return [...items, {id: id, quantity: item.quantity - 1}];
+            itemToReturn = [...items, {id: id, quantity: item.quantity - 1}];
+            itemToReturn.splice(0, 1);
+            return itemToReturn;
         }
-        return item;
+        return itemToReturn;
      });
      return updatedItems;
 }
