f267276ae991d1b0a77bd8d2df483ca16d33e691
Miguel Lopes Isidoro <miguel.lopes.isidoro@hotmail.com>
2023-06-29 17:58:11 +0100

    Continued dev

--- eCommerceSvelte/ecommercewebsite/src/routes/api/stripeCheckout/+server.ts
+++ eCommerceSvelte/ecommercewebsite/src/routes/api/stripeCheckout/+server.ts
@@ -1,2 +1,30 @@
 import type { RequestHandler } from "./$types";
 import Stripe from 'stripe'
+
+const STRIPE_API_KEY = "sk_test_51NONvLC892MWjQYTUg369LLlC91oUmUlLaLjisw5hS7h1eXow2C5oNoY9VAcUG5s1pq1PEdPQ2ISMDcfrKqLkr2E00nriAbJwn"
+const stripe = new Stripe(STRIPE_API_KEY, {
+    apiVersion: "2022-11-15"
+})
+
+export const POST: RequestHandler = async({request}) => {
+    const data = await request.json();
+    const items = data.items;
+    const lineItems: any = [];
+    items.forEach((item: any) => {
+        lineItems.push({price: item.id, quantity: item.quantity});
+    });
+    const session = await stripe.checkout.sessions.create({
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: "http://127.0.0.1:5173/success",
+        cancel_url: "http://127.0.0.1:5173/cancel"
+    });
+
+    return new Response(
+        JSON.stringify(({url: session.url})),
+        {
+            status: 200,
+            headers : {'content-type': 'application/json'}
+        }
+    );
+}
